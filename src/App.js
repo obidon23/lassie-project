e14150bae2563b467ccab0451882fd72be8dda43
obidon23 <dguck23@gmail.com>
2017-10-13 14:49:50 -0600

    first push of code

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+import Lassieform from './components/Lassieform.js';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container-fluid">
+        <Lassieform />
+
+      </div>
+    );
+  }
+}
+
+export default App;
